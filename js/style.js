7656fa4c0776447d963ac705b5cafa3f59ff5574
Bhavika Desai <desaibh@gmail.com>
2020-02-23 01:02:22 -0500

    commit canvas and pencil

--- js/style.js
+++ js/style.js
@@ -1,1 +1,37 @@
+function buildCanvas() {
 
+var canvas = document.getElementById("draw");
+var ctx = canvas.getContext("2d");
+var drawing = false;
+var lastX = 0;
+var lastY = 0;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#000000';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+function draw(e) {
+  if (!drawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX,lastY);
+  ctx.lineTo(e.offsetX,e.offsetY);
+  ctx.stroke();
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+}
+
+canvas.addEventListener('mousedown', function (e) {
+  drawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', function () { drawing = false; });
+canvas.addEventListener('mouseout', function () { drawing = false; });
+
+};
+
+buildCanvas();
